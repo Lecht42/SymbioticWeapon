f1dfd454bfbacdcd080123a075bb17621764bb50
Lecht42 <lecht42@gmail.com>
2023-05-28 18:42:18 +0300

    Added XML structure support for creating thing symbiont links between things

--- SymbioticWeapon/SimbiontWeapons.cs
+++ SymbioticWeapon/SimbiontWeapons.cs
@@ -10,29 +10,12 @@ namespace SimbiontWeapons
     public static class SimbiontWeapons
     {
         static readonly string ModName = "SimbiontWeapons";
-        static readonly ModContentPack Mod = LoadedModManager.RunningMods.Single((ModContentPack mod) => mod.Name == ModName);
-        static readonly IEnumerable<Def> AllDefs = Mod.AllDefs;
-        static readonly IEnumerable<SymbiotKindDef> KindSymbiontDefs = 
-            AllDefs.Where(def => def is SymbiotKindDef sDef && sDef.pairId != null).Cast<SymbiotKindDef>();
-        static readonly IEnumerable<SymbiotThingDef> ThingSymbiontDefs =
-            AllDefs.Where(def => def is SymbiotThingDef sDef && sDef.pairId != null).Cast<SymbiotThingDef>();
-        static IEnumerable<PawnSymbiont> SimbiontDefs = new PawnSymbiont[0];
+        static public readonly IEnumerable<Def> AllDefs = LoadedModManager.RunningMods.Single((ModContentPack mod) => mod.Name == ModName).AllDefs;
+        static readonly IEnumerable<SymbiontDef> SimbiontDefs = AllDefs.Where(def => def is SymbiontDef).Cast<SymbiontDef>();
 
         static SimbiontWeapons()
-        { 
-            var pairedDefs = KindSymbiontDefs.Join(
-            ThingSymbiontDefs,
-            kindDef => kindDef.pairId,
-            thingDef => thingDef.pairId,
-            (kindDef, thingDef) => {
-                SimbiontDefs.AddItem(new PawnSymbiont(kindDef, thingDef));
-                return string.Concat(kindDef.defName, " - ", thingDef.defName);
-             });
+        {
 
-
-            foreach (var def in pairedDefs) {
-                Log.Message(def);
-            }
         }
     }
 }
